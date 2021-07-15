1ef5f5ab74bf6e7a112c3fbd564685c0cebcc717
jensengbg-jack-carling <72305598+jensengbg-jack-carling@users.noreply.github.com>
2021-07-15 20:27:34 +0200

    Delete directory

--- components/Header.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-import './Header.css';
-
-function Header() {
-  return <h1 className="title">SHOPPING LIST</h1>;
-}
-
-export default Header;
--- components/Main.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-function Main() {
-  return <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium, quas.</p>;
-}
-
-export default Main;

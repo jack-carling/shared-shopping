1b937ab0ddd43462e21a584dd38ff0a9fc824362
jensengbg-jack-carling <72305598+jensengbg-jack-carling@users.noreply.github.com>
2021-07-15 20:30:27 +0200

    Update directory

--- App.tsx
+++ App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
-import Header from './components/Header';
-import Main from './components/Main';
+import Header from './src/components/Header';
+import Main from './src/components/Main';
 
 function App() {
   return (
